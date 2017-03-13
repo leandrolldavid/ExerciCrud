68db7846e9c0fb6a68c0449e296b3d3537461706
leandrolldavid <leandrolldavid@gmail.com>
2017-03-13 20:56:51 -0300

    save 2

--- ExerInsPesqHibernate/src/java/Produto.java
+++ ExerInsPesqHibernate/src/java/Produto.java
@@ -1,8 +1,10 @@
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,9 +21,13 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //ID automatico
     private int codigo;
+    @Column(name = "produto")
     private String nome;
     private int valor;
-
+     
+    @ManyToOne//descrição de relacionamento
+    private Cliente cliente;
+            
     public int getCodigo() {
         return codigo;
     }
@@ -44,5 +50,13 @@ public class Produto {
 
     public void setValor(int valor) {
         this.valor = valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 }
